00011c1b97727af19f14de7a082681542cea0386
d-plaindoux <d.plaindoux@free.fr>
2024-08-09 11:13:17 +0200

    Ephel lang v0 backend tests

--- stage/v0/lib/2-compiler/ephel_compiler.mli
+++ stage/v0/lib/2-compiler/ephel_compiler.mli
@@ -5,3 +5,4 @@ module Objcode = Ephel_compiler_objcode
 module Transpiler = Ephel_compiler_transpiler
 module Expander = Ephel_compiler_expander
 module Optimiser = Ephel_compiler_optimiser
+module Simplifier = Ephel_compiler_simplifier
---
+++ stage/v0/test/9-simplifier/t00_simplifier_tests.ml
@@ -0,0 +1,4 @@
+let () =
+  Alcotest.(
+    run "Simplifier Test"
+      [ T01_basic.cases; T02_sum.cases; T03_pair.cases; T04_lambda.cases ] )
